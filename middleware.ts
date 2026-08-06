0d2b4e1cb07018ed0bfc504093a3bd6c370ac706
Jonas <huoju@madmod.cn>
2026-08-06 15:33:53 +0800

    Revert "fix: exclude api/test-connection from NextAuth middleware"
    This reverts commit 8d1a042744dd7139dda973c431db3740048ccc2f.

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,12 @@
-import NextAuth from "next-auth";
-import { authConfig } from "@/lib/auth.config";
+import NextAuth from "next-auth"
+import { authConfig } from "@/lib/auth.config"
 
-const { auth } = NextAuth(authConfig);
+const { auth } = NextAuth(authConfig)
 
-export default auth;
+export default auth
 
 export const config = {
   matcher: [
-    "/((?!api/auth|api/providers|api/test-connection|_next/static|_next/image|favicon.ico|public).*)",
-  ],
-};
+    "/((?!api/auth|api/providers|_next/static|_next/image|favicon.ico|public).*)"
+  ]
+}
